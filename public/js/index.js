6c0540f272a8ae98e83ca94edc9aec2689a04243
yeramwap <priyanka.yeramwar@travelex.com>
2019-08-23 15:43:25 +0530

    Add geolocation support

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,7 @@ var socket = io();
 socket.on('connect',function() {
  console.log('connected to server');
 
-//  socket.emit('createMessage',{
-//     from:'Pritish',
-//     text:'hi....'
-//  });
+
 });
 
 socket.on('disconnect',function() {
@@ -15,4 +12,51 @@ socket.on('disconnect',function() {
 
 socket.on('newMessage', function(message) {
     console.log('new Message',message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function(message) {    
+    var li = jQuery('<li></li>');
+    var a= jQuery('<a target="_blank">My current location</a>');
+
+    li.text(`${message.from}: `);
+    a.attr('href',message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+});
+
+//  socket.emit('createMessage',{
+//     from:'Pritish',
+//     text:'hi 1234....'
+//  },function(data) {
+//      console.log('got the message..'+data);
+//  });
+
+ jQuery('#message-form').on('submit', function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage',{
+        from:jQuery('[name=userName]').val(),
+        text:jQuery('[name=message]').val()
+     },function() {
+         console.log('got the message..');
+     });
+ });
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click',function (){
+ if (!navigator.geolocation) {
+    return alert('Geolocation not supported by browser');
+ }
+ navigator.geolocation.getCurrentPosition(function(position){
+    socket.emit('createLocationMessage',{
+        latitude:position.coords.latitude,
+        longitude:position.coords.longitude
+    });
+ },function(){
+    alert('unable to fetch location');
+})
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {generateMessage} =require('./message');
+const {generateMessage,generateLocationMessage} =require('./message');
 
 describe('generateMessage',() =>{
     it('should generate a message' ,() =>{
@@ -10,6 +10,20 @@ describe('generateMessage',() =>{
 
        // expect(message.createdAt).toBe('number');
         expect(message.text).toBe(text);
+        expect(message.from).toBe(from);
+    });    
+});
+
+describe('generateLocationMessage',() =>{
+    it('should generate correct location object' ,() =>{
+        var from='Priyanka';
+        var latitude = 13;
+        var longitude = 35;
+        var url = 'https://google.com/maps?q=13,35';
+        var message  = generateLocationMessage(from,latitude,longitude);
+
+       // expect(message.createdAt).toBe('number');
+        expect(message.url).toBe(url);
         expect(message.from).toBe(from);
     });    
 });
